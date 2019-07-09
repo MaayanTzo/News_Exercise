5c10d26e05bd4bc17640cb7b3be5fc55d0d22feb
Maayan Tzoubari <maayan.tzoubari@gmail.com>
2019-07-09 20:38:22 +0200

    completed

---
+++ __init__.py
@@ -0,0 +1,24 @@
+from bottle import *
+import feedparser
+import json
+
+
+@route('/')
+def index():
+    return static_file("star2.html", root='')
+
+@route('/<filename:re:.*\.css>')
+def stylesheets(filename):
+    return static_file(filename, root='')
+
+@route('/getArticles')
+def get_articles():
+    feed = feedparser.parse("https://www.jpost.com/Rss/RssFeedsHeadlines.aspx")
+    articles=[]
+    for i in range(1,20):
+        article = {"title": feed["entries"][i]["title"], "link": feed["entries"][i]["link"]}
+        articles.append(article)
+    return json.dumps(articles)
+
+if __name__ == '__main__':
+    run(host='localhost',port=7000, debug=True)
